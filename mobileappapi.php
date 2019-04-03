4e860914b7781fe764dc3613a4766427a2d938f0
Jamtech Technologies Private Limited <ajayphpstudy@gmail.com>
2019-04-03 13:15:08 +0530

    Update mobileappapi.php

--- mobileappapi.php
+++ mobileappapi.php
@@ -16,6 +16,22 @@ if (!defined('WPINC')) {
     die;
 }
 
+
+add_action( 'rest_api_init', function() {
+    
+	remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+	add_filter( 'rest_pre_serve_request', function( $value ) {
+		header( 'Access-Control-Allow-Origin: *' );
+		header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE' );
+		header( 'Access-Control-Allow-Credentials: true' );
+
+		return $value;
+		
+	});
+}, 15 );
+
+
+
 function test_jwt_auth_expire($issuedAt)
 {
     return $issuedAt + (DAY_IN_SECONDS * 10000);
